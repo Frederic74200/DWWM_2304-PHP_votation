78735e9b60cde8baa027b7013d419edf6ce9ed4d
Mdevoldere306 <mdevoldere@users.noreply.github.com>
2023-11-16 16:45:03 +0100

    Create 01-hello.php

---
+++ exercices-bases/01-hello.php
@@ -0,0 +1,19 @@
+<?php 
+
+function helloWorld() : void 
+{
+    echo "Hello World !";
+}
+
+helloWorld();
+
+
+function hello(string $name): string
+{
+    return "Hello $name";
+}
+
+$resultat = hello("Mike");
+echo $resultat;
+
+echo hello("Toto");
